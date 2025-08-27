66bad580929c94fafac5c7aa4ceb84b77b3688d8
sujayyy <150987692+sujaytumu@users.noreply.github.com>
2025-08-27 20:00:24 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
+          "Software Developer
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
